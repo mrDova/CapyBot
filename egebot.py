ac460c70c64f99c0cbf2ed1a95a7e5e354b99253
mrDova <139633509+mrDova@users.noreply.github.com>
2023-07-19 22:43:48 +0700

    Update egebot.py

--- egebot.py
+++ egebot.py
@@ -30,10 +30,10 @@ def nachalo(message):
 def vcurs(message):
     if message.text == 'Мой Курс❤️':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('Занятие 1')
-        btn2 = types.KeyboardButton('Занятие 2')
-        btn3 = types.KeyboardButton('Занятие 3')
-        btn4 = types.KeyboardButton('Занятие 4')
+        btn1 = types.KeyboardButton('Занятие 1 21.07.2023')
+        btn2 = types.KeyboardButton('Занятие 2 25.07.2023')
+        btn3 = types.KeyboardButton('Занятие 3 28.07.2023')
+        btn4 = types.KeyboardButton('Занятие 4 01.08.2023')
         markup.row(btn1)
         markup.row(btn2)
         markup.row(btn3)
@@ -43,7 +43,7 @@ def vcurs(message):
         bot.register_next_step_handler(message, zad)
     elif message.text == 'Беседа Летнего Курса ЕГЭ':
         markup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton('Ссылка на беседу', url='https://t.me/+Dd9o9PDg-vs5ZWEy')
+        btn1 = types.InlineKeyboardButton('Ссылка на беседу', url='https://t.me/+modSPJz7XQMyYmQy')
         markup.row(btn1)
         bot.send_message(message.chat.id, 'Это  чат с твоим преподавателем и однокурсниками. Ко времени онлайн занятий тут будут приходить ссылки на них. А еще тут можно что-то обсудить, поделиться впечатлениями и новостями❤️', reply_markup=markup)
         bot.register_next_step_handler(message, vcurs)
@@ -55,6 +55,12 @@ def vcurs(message):
         bot.register_next_step_handler(message, vcurs)
     elif message.text == '/restart':
         bot.register_next_step_handler(message, start)
+    elif message.text == '/link':
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton('Ссылка на беседу', url='https://t.me/+modSPJz7XQMyYmQy')
+        markup.row(btn1)
+        bot.send_message(message.chat.id, 'Это  чат с твоим преподавателем и однокурсниками. Ко времени онлайн занятий тут будут приходить ссылки на них. А еще тут можно что-то обсудить, поделиться впечатлениями и новостями❤️', reply_markup=markup)
+        bot.register_next_step_handler(message, vcurs)
     else:
         bot.send_message(message.chat.id, 'Не совсем понял тебя... Давай попоробуем ещё раз')
         bot.register_next_step_handler(message, vcurs)
@@ -63,7 +69,7 @@ def vcurs(message):
 
 
 def zad(message):
-    if message.text == 'Занятие 1':
+    if message.text == 'Занятие 1 21.07.2023':
         markup = types.InlineKeyboardMarkup()
         markups = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.InlineKeyboardButton('Скрипт', callback_data='skrp1')
@@ -74,7 +80,7 @@ def zad(message):
         bot.send_message(message.chat.id, f'Занятие 1 <b>«ВВЕДЕНИЕ В ЦИТОЛОГИЮ»</b> \n <i>Дата онлайн-занятия:21.07.2023</i> \n <i>Время: 10:00 мск</i> \n ( Запись появится после трансляции, а пока распечатай скрипт и насладись видосиком с Капибарами ) \n https://youtu.be/wcov8v0hrHY', reply_markup=markup, parse_mode="html")
         bot.send_message(message.chat.id, 'Смотри, учи, запоминай!', reply_markup=markups)
         bot.register_next_step_handler(message, hlp)
-    elif message.text == 'Занятие 2':
+    elif message.text == 'Занятие 2 25.07.2023': 
         markup = types.InlineKeyboardMarkup()
         markups = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.InlineKeyboardButton('Скрипт', callback_data='skrp2')
@@ -85,7 +91,7 @@ def zad(message):
         bot.send_message(message.chat.id, f'Занятие 2 <b>«КЛЕТОЧНЫЙ ЦИКЛ.МИТОЗ»</b> \n <i>Дата онлайн-занятия:25.07.2023</i> \n <i>Время: 10:00 мск</i> \n ( Запись появится после трансляции, а пока распечатай скрипт и насладись видосиком с Капибарами ) \n https://youtu.be/wcov8v0hrHY', reply_markup=markup, parse_mode="html")
         bot.send_message(message.chat.id, 'Смотри, учи, запоминай!', reply_markup=markups)
         bot.register_next_step_handler(message, hlp)
-    elif message.text == 'Занятие 3':
+    elif message.text == 'Занятие 3 28.07.2023':
         markup = types.InlineKeyboardMarkup()
         markups = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.InlineKeyboardButton('Скрипт', callback_data='skrp3')
@@ -96,7 +102,7 @@ def zad(message):
         bot.send_message(message.chat.id, f'Занятие 3 <b>«МЕЙОЗ.ГАМЕТОГЕНЕЗ»</b> \n <i>Дата онлайн-занятия:28.07.2023</i> \n <i>Время: 10:00 мск</i> \n ( Запись появится после трансляции, а пока распечатай скрипт и насладись видосиком с Капибарами ) \n https://youtu.be/wcov8v0hrHY', reply_markup=markup, parse_mode="html")
         bot.send_message(message.chat.id, 'Смотри, учи, запоминай!', reply_markup=markups)
         bot.register_next_step_handler(message, hlp)
-    elif message.text == 'Занятие 4':
+    elif message.text == 'Занятие 4 01.08.2023':
         markup = types.InlineKeyboardMarkup()
         markups = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.InlineKeyboardButton('Скрипт', callback_data='skrp4')
@@ -107,6 +113,12 @@ def zad(message):
         bot.send_message(message.chat.id, f'Занятие 4 <b>«ЦИКЛЫ РАСТЕНИЙ»</b> \n <i>Дата онлайн-занятия:01.08.2023</i> \n <i>Время: 10:00 мск</i> \n ( Запись появится после трансляции, а пока распечатай скрипт и насладись видосиком с Капибарами ) \n https://youtu.be/wcov8v0hrHY', reply_markup=markup, parse_mode="html")
         bot.send_message(message.chat.id, 'Смотри, учи, запоминай!', reply_markup=markups)
         bot.register_next_step_handler(message, hlp)
+    elif message.text == '/link':
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton('Ссылка на беседу', url='https://t.me/+modSPJz7XQMyYmQy')
+        markup.row(btn1)
+        bot.send_message(message.chat.id, 'Это  чат с твоим преподавателем и однокурсниками. Ко времени онлайн занятий тут будут приходить ссылки на них. А еще тут можно что-то обсудить, поделиться впечатлениями и новостями❤️', reply_markup=markup)
+        bot.register_next_step_handler(message, zad)
     else:
         bot.send_message(message.chat.id, 'Не совсем понял тебя... Давай попоробуем ещё раз')
         bot.register_next_step_handler(message, zad)
@@ -114,10 +126,10 @@ def zad(message):
 def hlp(message):
     if message.text == 'Выбрать другое занятие':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('Занятие 1')
-        btn2 = types.KeyboardButton('Занятие 2')
-        btn3 = types.KeyboardButton('Занятие 3')
-        btn4 = types.KeyboardButton('Занятие 4')
+        btn1 = types.KeyboardButton('Занятие 1 21.07.2023')
+        btn2 = types.KeyboardButton('Занятие 2 25.07.2023')
+        btn3 = types.KeyboardButton('Занятие 3 28.07.2023')
+        btn4 = types.KeyboardButton('Занятие 4 01.08.2023')
         markup.row(btn1)
         markup.row(btn2)
         markup.row(btn3)
@@ -127,6 +139,12 @@ def hlp(message):
         bot.register_next_step_handler(message, zad)
     elif message.text == '/restart':
         bot.register_next_step_handler(message, start)
+    elif message.text == '/link':
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton('Ссылка на беседу', url='https://t.me/+modSPJz7XQMyYmQy')
+        markup.row(btn1)
+        bot.send_message(message.chat.id, 'Это  чат с твоим преподавателем и однокурсниками. Ко времени онлайн занятий тут будут приходить ссылки на них. А еще тут можно что-то обсудить, поделиться впечатлениями и новостями❤️', reply_markup=markup)
+        bot.register_next_step_handler(message, hlp)
     else:
         bot.send_message(message.chat.id, 'Не совсем понял тебя... Давай попоробуем ещё раз')
         bot.register_next_step_handler(message, hlp)
@@ -140,7 +158,7 @@ def callback_message(callback):
         markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton('Отправить ДЗ на проверку', url='https://t.me/baisov_islam')
         markup.row(btn1)
-        bot.send_message(callback.message.chat.id, 'Вот ссылка на твоё дз:\n https://docs.google.com/forms/d/e/1FAIpQLSf-pKKI_n_VT5ivjrvWy_UetjP2aROYkML-7FecAuSMJvX3bw/viewform?usp=share_link', reply_markup=markup)
+        bot.send_message(callback.message.chat.id, f'<a href="https://docs.google.com/forms/d/e/1FAIpQLSf-pKKI_n_VT5ivjrvWy_UetjP2aROYkML-7FecAuSMJvX3bw/viewform?usp=share_link">Вот ссылка на дз к занятию 1</a>', reply_markup=markup, parse_mode="html")
     if callback.data == 'dz2':
         markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton('Отправить ДЗ на проверку', url='https://t.me/baisov_islam')
@@ -157,14 +175,27 @@ def callback_message(callback):
         markup.row(btn1)
         bot.send_message(callback.message.chat.id, '( Дз скоро появится, а пока  насладись видосиком с Капибарами): https://youtu.be/wcov8v0hrHY ', reply_markup=markup)
     if callback.data == 'skrp1':
-        bot.send_message(callback.message.chat.id, 'Вот ссылка на скрипт:\n https://drive.google.com/file/d/1awCmoCCKm_lPLCgYOCZ_swop9AhToYl9/view?usp=share_link')
+        bot.send_message(callback.message.chat.id, '<a href="https://drive.google.com/file/d/1awCmoCCKm_lPLCgYOCZ_swop9AhToYl9/view?usp=share_link">Вот ссылка на скрипт к занятию 1</a>:\n Обязательно распечатай скрипт перед занятием', parse_mode="html")
     if callback.data == 'skrp2':
         bot.send_message(callback.message.chat.id, '( Скрипт скоро появится, а пока  насладись видосиком с Капибарами): https://youtu.be/wcov8v0hrHY ')
     if callback.data == 'skrp3':
         bot.send_message(callback.message.chat.id, '( Скрипт скоро появится, а пока  насладись видосиком с Капибарами): https://youtu.be/wcov8v0hrHY ')
     if callback.data == 'skrp4':
         bot.send_message(callback.message.chat.id, '( Скрипт скоро появится, а пока  насладись видосиком с Капибарами): https://youtu.be/wcov8v0hrHY ')
+    elif message.text == '/link':
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton('Ссылка на беседу', url='https://t.me/+modSPJz7XQMyYmQy')
+        markup.row(btn1)
+        bot.send_message(message.chat.id, 'Это  чат с твоим преподавателем и однокурсниками. Ко времени онлайн занятий тут будут приходить ссылки на них. А еще тут можно что-то обсудить, поделиться впечатлениями и новостями❤️', reply_markup=markup)
+        bot.register_next_step_handler(message, hlp)
 
+
+def link(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Ссылка на беседу', url='https://t.me/+modSPJz7XQMyYmQy')
+    markup.row(btn1)
+    bot.send_message(message.chat.id, 'Это  чат с твоим преподавателем и однокурсниками. Ко времени онлайн занятий тут будут приходить ссылки на них. А еще тут можно что-то обсудить, поделиться впечатлениями и новостями❤️', reply_markup=markup)
+    bot.register_next_step_handler(message, hlp)
 
     
 
